3731766234621efddc6c8debc5c5ffb79ef02769
AleCaderudo <139296557+AleCaderudo@users.noreply.github.com>
2024-07-13 10:47:32 -0300

    Update index.js

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,10 +1,9 @@
 import express from "express";
 import produtos from "./produtosRoutes.js";
 import clientes from "./clientesRoutes.js";
-const path = require('path');
 
 const routes = (app) => {
-  app.route("/").get((req, res) => res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+  app.route("/").get((req, res) => res.status(200).send("Cosmeticosecia"));
 
   app.use(express.json(), produtos, clientes);
 };

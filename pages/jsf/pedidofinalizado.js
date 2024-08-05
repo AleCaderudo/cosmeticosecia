ff3d82b0ba4a9027c8b561604afce03124841721
AleCaderudo <139296557+AleCaderudo@users.noreply.github.com>
2024-08-05 09:59:30 -0300

    Add files via upload

--- pages/jsf/pedidofinalizado.js
+++ pages/jsf/pedidofinalizado.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', async function () {
     const detalhesPedido = document.getElementById('detalhesPedido');
     const pedido = JSON.parse(localStorage.getItem('pedido'));
-    
+    const dadosUser = JSON.parse(localStorage.getItem('loggedInUser'));
+
     if (pedido) {
         const {
             nome,
@@ -16,13 +17,12 @@ document.addEventListener('DOMContentLoaded', async function () {
         let detalhesHtml = `
             <div class="tabela__compras__finalizadas">
                 <div class="fonte__compras">
-                    <p>Caro ${nome}, seu pedido foi processado a assim que confirmarmos o pagamento ele será enviado para:</p><br>
+                    <p>Caro ${nome}, seu pedido foi processado e assim que confirmarmos o pagamento ele será enviado para:</p><br>
                     <p><strong>Endereço:</strong> ${endereco}</p>
                     <p><strong>Cidade:</strong> ${cidade}</p>
                     <p><strong>Estado:</strong> ${estado}</p>
                     <p><strong>CEP:</strong> ${cep}</p><br><br>
                     <p><strong>Forma de Pagamento escolhida:</strong> ${formaPagamento}</p><br>
-
                 </div>    
             </div>    
         `;
@@ -38,16 +38,14 @@ document.addEventListener('DOMContentLoaded', async function () {
             detalhesHtml += `
                 <div class="tabela__compras__finalizadas">
                     <div class="fonte__compras">
-                        </div>
-                    </div>    
-                </div>   
+                    </div>
+                </div>    
             `;
         } else if (formaPagamento === 'boleto') {
             detalhesHtml += `
                 <div class="tabela__compras__finalizadas">
                     <div class="fonte__compras">
-                        </div>
-                    </div>    
+                    </div>
                 </div>    
             `;
         }
@@ -67,10 +65,10 @@ document.addEventListener('DOMContentLoaded', async function () {
                 produtos.forEach(produto => {
                     total += produto.preco * 1;
                     detalhesHtml += `
-                <div class="card__compras">
-                    <img class="img__compras" src="../img/produtos/${produto.imagem}">
-                    <div class="fonte__compras"><strong>Nome:</strong> ${produto.nome} - <strong> Preço:</strong> R$ ${produto.preco}</div>
-                </div><br><br>
+                        <div class="card__compras">
+                            <img class="img__compras" src="../img/produtos/${produto.imagem}">
+                            <div class="fonte__compras"><strong>Nome:</strong> ${produto.nome} - <strong> Preço:</strong> R$ ${produto.preco}</div>
+                        </div><br><br>
                     `;
                 });
 
@@ -80,6 +78,9 @@ document.addEventListener('DOMContentLoaded', async function () {
                         </div>    
                     </div>    
                 `;
+                detalhesPedido.innerHTML = detalhesHtml;
+
+                deletaPedido();
             } catch (error) {
                 console.error('Erro ao buscar produtos:', error);
                 detalhesHtml += `
@@ -89,6 +90,7 @@ document.addEventListener('DOMContentLoaded', async function () {
                         </div>    
                     </div>    
                 `;
+                detalhesPedido.innerHTML = detalhesHtml;
             }
         } else {
             detalhesHtml += `
@@ -98,9 +100,30 @@ document.addEventListener('DOMContentLoaded', async function () {
                     </div>    
                 </div>    
             `;
+            detalhesPedido.innerHTML = detalhesHtml;
         }
 
-        detalhesPedido.innerHTML = detalhesHtml;
+        async function deletaPedido() {
+            try {
+                const response = await fetch(`https://cosmback.vercel.app/clientes/${dadosUser._id}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        compras: []
+                    })
+                });
+
+                if (response.ok) {
+                    console.log('Compras deletadas com sucesso.');
+                } else {
+                    console.error('Erro ao deletar compras:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Erro ao deletar compras:', error);
+            }
+        }
     } else {
         detalhesPedido.innerHTML = '<p>Nenhum pedido encontrado.</p>';
     }
